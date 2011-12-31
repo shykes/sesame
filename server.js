25a5df17b86b2c1b5c347e2f0e6393b9288dc701
Solomon Hykes <solomon@dotcloud.com>
2011-12-31 14:57:17 -0800

    Moved Twilio markup to a separate file

--- server.js
+++ server.js
@@ -1,10 +1,17 @@
 
 var http = require('http');
+var fs = require('fs');
 
 var server = http.createServer(function (request, response) {
-          response.writeHead(200, {"Content-Type": "text/xml"});
-            response.end("<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<Response><Say>Hello Coco Hello Coco Hello Coco</Say></Response>");
-            });
+        fs.readFile('./voice', function (error, content) {
+            if (error) {
+            }
+            else {
+                response.writeHead(200, {"Content-Type": "text/xml"});
+                response.end(content, 'utf-8');
+            }
+        });
+});
 
 // Listen on port 8000, IP defaults to 127.0.0.1
 server.listen(8080);
