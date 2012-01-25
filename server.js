745e7252184096d7ce78317f094fec62177345b9
Solomon Hykes <solomon@dotcloud.com>
2012-01-24 19:21:15 -0800

    A basic but functional implementation of a keypad pincode system

--- server.js
+++ server.js
@@ -1,16 +1,58 @@
 
 var http = require('http');
-var fs = require('fs');
+
+var serve = function (path, response, content_type) {
+    require('fs').readFile(path, function(error, content) {
+        if (error) {
+        }
+        else {
+            response.writeHead(200, {"Content-Type": content_type ? content_type : "text/xml"});
+            response.end(content, 'utf-8');
+        }
+    });
+}
+
+var redirect = function (path, response) {
+    response.writeHead(302, {'Location': path});
+    response.end();
+    }
 
 var server = http.createServer(function (request, response) {
-        fs.readFile('./voice', function (error, content) {
-            if (error) {
+        var pathname = require('url').parse(request.url).pathname;
+        console.log(pathname);
+        if (pathname == '/voice') {
+            serve('./voice', response);
+        }
+        else if (pathname == '/pin') {
+            var digits = require('querystring').parse(request.url).Digits;
+            // ... Check for valid pin code
+            if (digits) {
+                console.log("Digits: " + digits);
+                if (digits == '4321') {
+                    serve('./code_ok', response);
+                }
+                else if (digits == '42424242') {
+                    serve('./record', response);
+                }
+                else {
+                    serve('./code_error', response);
+                }
             }
             else {
-                response.writeHead(200, {"Content-Type": "text/xml"});
-                response.end(content, 'utf-8');
+                serve('./code_error'); 
             }
-        });
+        }
+        else if (pathname == '/new_recording') {
+            var recordingUrl = require('querystring').parse(request.url).RecordingUrl;
+            console.log("New recording: " + recordingUrl); 
+            redirect('/voice', response);
+        }
+        else {
+            match = new RegExp(/\/(.*\.wav)$/).exec(pathname);
+            if (match) {
+                serve(match[1], response, 'audio/wav');
+            }
+        }
 });
 
 // Listen on port 8000, IP defaults to 127.0.0.1
